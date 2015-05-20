c0f02313cafbd5cc8d88fdfbcfb6d64073657edd
Juan Ruiz <juanruizm001@gmail.com>
2015-05-20 18:41:37 -0300

    Mejorando archivo php

--- helloworld.php
+++ helloworld.php
@@ -1,5 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>
+
+    </title>
+</head>
+<body>
+<h1>Curso de PHP</h1>
 <?php
-
-echo "Hello World";
-
+    echo "Hello World";
 ?>
+</body>
+</html>
+
